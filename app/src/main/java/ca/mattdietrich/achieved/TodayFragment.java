7c6eab678ab17c26f3388d76887792e7b9ac3261
Matt Dietrich <mdietr@alumni.uwo.ca>
2015-06-27 14:01:56 -0400

    Initial commit

---
+++ app/src/main/java/ca/mattdietrich/achieved/TodayFragment.java
@@ -0,0 +1,98 @@
+package ca.mattdietrich.achieved;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link TodayFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link TodayFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TodayFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment TodayFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static TodayFragment newInstance(int navDrawerSectionNumber) {
+        TodayFragment fragment = new TodayFragment();
+        Bundle args = new Bundle();
+        args.putInt (MainActivity.ARG_NAV_DRAWER_SECTION_NUMBER, navDrawerSectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public TodayFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_today, container, false);
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(MainActivity.ARG_NAV_DRAWER_SECTION_NUMBER));
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
